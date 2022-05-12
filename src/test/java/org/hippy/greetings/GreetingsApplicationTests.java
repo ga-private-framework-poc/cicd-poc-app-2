16afba3517d1d28bd7e7964f74538a8a1c5f095b
Evan A Slatis <eslatis@redhat.com>
2022-05-12 18:58:45 -0500

    initial commit of new framework

---
+++ src/test/java/org/hippy/greetings/GreetingsApplicationTests.java
@@ -0,0 +1,15 @@
+package org.hippy.greetings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class GreetingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
